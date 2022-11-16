029e9a6d2825ed78adea14f27a9b195f6ad1be54
patrosk <naiade99@gmail.com>
2022-11-16 11:49:04 +0100

    add people schema

---
+++ backend/schemas/documents/people/people.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'people',
+    type: 'document',
+    title: 'People',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+    ],
+};
